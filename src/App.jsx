ab73dc1e1ae5d7a3933b5f0bd4f2eab52085e33f
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-03 14:46:43 +0200

    [upd] added static form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,81 @@
 function App() {
   return (
     <>
-    <h2>Hello World</h2>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <form>
+              <div class="row">
+                <div class="col">
+                  <label for="exampleFormControlTextarea1" class="form-label">
+                    Author Name
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Insert author's name.."
+                    aria-label="Author Name"
+                  />
+                </div>
+                <div class="col">
+                  <label for="exampleFormControlTextarea1" class="form-label">
+                    Post Title
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Insert post title.."
+                    aria-label="Post Title"
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="exampleFormControlTextarea1" class="form-label">
+                    Post Content
+                  </label>
+                  <textarea
+                    class="form-control"
+                    placeholder="Type here.."
+                    id="exampleFormControlTextarea1"
+                    rows="3"
+                  ></textarea>
+                </div>
+              </div>
+              <fieldset className="row mb-3">
+                <legend className="col-form-label col-sm-2 pt-0">
+                  Public Post
+                </legend>
+                <div className="col-sm-10">
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="gridRadios"
+                      id="gridRadios1"
+                      value="option1"
+                      checked
+                    />
+                    <label className="form-check-label" for="gridRadios1">
+                      Yes
+                    </label>
+                  </div>
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="gridRadios"
+                      id="gridRadios2"
+                      value="option2"
+                    />
+                    <label className="form-check-label" for="gridRadios2">
+                      No
+                    </label>
+                  </div>
+                </div>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
