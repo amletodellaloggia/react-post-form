2c3743620b3918f6c5b56c4772b9db0b2ab49e44
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-03 19:01:39 +0200

    [upd] rendered radio button type/value, included handleSubmit to form, changed values to tags, included formData in values

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -15,7 +15,7 @@ const PostForm = () => {
     const { name, value, type } = e.target;
     setFormData({
       ...formData,
-      [name]: type === "radio" ? value === "true" : value,
+      [name]: type === "radio" ? (value === "true") : value,
     });
   };
 
@@ -31,91 +31,98 @@ const PostForm = () => {
   };
 
   return (
-    <div>
-      <div className="container-fluid m-5">
-        <div className="row">
-          <div className="col">
-            <form>
-              <h2 className="mb-3">Create New Post</h2>
-              <div className="row">
-                <div className="col">
-                  <label
-                    htmlFor="exampleFormControlTextarea1"
-                    className="form-label"
-                  >
-                    Author Name
-                  </label>
+    <div className="container-fluid m-5">
+      <div className="row">
+        <div className="col">
+          <form onSubmit={handleSubmit}>
+            <h2 className="mb-3">Create New Post</h2>
+
+            <div className="row">
+              <div className="col">
+                <label htmlFor="author" className="form-label">
+                  Author Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Insert author's name.."
+                  id="author"
+                  name="author"
+                  value={formData.author}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="col mb-3">
+                <label htmlFor="title" className="form-label">
+                  Post Title
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Insert post title.."
+                  id="title"
+                  name="title"
+                  value={formData.title}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="body" className="form-label">
+                  Post Content
+                </label>
+                <textarea
+                  className="form-control"
+                  placeholder="Type here.."
+                  id="body"
+                  name="body"
+                  rows="3"
+                  value={formData.body}
+                  onChange={handleChange}
+                ></textarea>
+              </div>
+            </div>
+
+            <fieldset className="row mb-3">
+              <legend className="col-form-label col-sm-2 pt-0">Public</legend>
+              <div className="col-sm-10">
+                <div className="form-check">
                   <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Insert author's name.."
-                    aria-label="Author Name"
+                    className="form-check-input"
+                    type="radio"
+                    name="public"
+                    id="publicYes"
+                    value="true"
+                    checked={formData.public === true}
+                    onChange={handleChange}
                   />
-                </div>
-                <div className="col mb-3">
-                  <label
-                    htmlFor="exampleFormControlTextarea2"
-                    className="form-label"
-                  >
-                    Post Title
+                  <label className="form-check-label" htmlFor="publicYes">
+                    Yes
                   </label>
+                </div>
+
+                <div className="form-check">
                   <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Insert post title.."
-                    aria-label="Post Title"
+                    className="form-check-input"
+                    type="radio"
+                    name="public"
+                    id="publicNo"
+                    value="false"
+                    checked={formData.public === false}
+                    onChange={handleChange}
                   />
-                </div>
-                <div className="mb-3">
-                  <label
-                    htmlFor="exampleFormControlTextarea3"
-                    className="form-label"
-                  >
-                    Post Content
+                  <label className="form-check-label" htmlFor="publicNo">
+                    No
                   </label>
-                  <textarea
-                    className="form-control"
-                    placeholder="Type here.."
-                    id="exampleFormControlTextarea3"
-                    rows="3"
-                  ></textarea>
                 </div>
               </div>
-              <fieldset className="row mb-3">
-                <legend className="col-form-label col-sm-2 pt-0">Public</legend>
-                <div className="col-sm-10">
-                  <div className="form-check">
-                    <input
-                      className="form-check-input"
-                      type="radio"
-                      name="gridRadios"
-                      id="gridRadios1"
-                      value="option1"
-                      defaultChecked
-                    />
-                    <label className="form-check-label" htmlFor="gridRadios1">
-                      Yes
-                    </label>
-                  </div>
-                  <div className="form-check">
-                    <input
-                      className="form-check-input"
-                      type="radio"
-                      name="gridRadios"
-                      id="gridRadios2"
-                      value="option2"
-                    />
-                    <label className="form-check-label" htmlFor="gridRadios2">
-                      No
-                    </label>
-                  </div>
-                </div>
-              </fieldset>
-              <button type="submit" className="btn btn-primary">
-                Submit Post
-              </button>
-            </form>
-          </div>
+            </fieldset>
+
+            <button type="submit" className="btn btn-primary">
+              Submit Post
+            </button>
+          </form>
         </div>
       </div>
     </div>
